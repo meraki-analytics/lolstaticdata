a63dcc385b6aa3d4c88d75d650f472d0d556b40c
Paaksing Tech <58193352+paaksing@users.noreply.github.com>
2020-05-24 18:05:38 -0400

    Updated get_ability_url function to use cdn
    cdn doesn't change path to images, and better to maintain

--- lolstaticdata/champions/pull_champions_dragons.py
+++ lolstaticdata/champions/pull_champions_dragons.py
@@ -72,71 +72,134 @@ def perform_guess(
 
 
 def get_ability_url(key, ability_key, ability_index, ability_name, latest_version, ddragon_champion, filenames):
+    return f"https://cdn.communitydragon.org/latest/champion/{key}/ability-icon/{ability_key[0]}"
     # Use ddragon if we can
+    """
     if ability_index == 1 and ability_key in ("Q", "W", "E", "R"):
         spell_index = {"Q": 0, "W": 1, "E": 2, "R": 3}[ability_key]
         return f"https://ddragon.leagueoflegends.com/cdn/{latest_version}/img/spell/{ddragon_champion['spells'][spell_index]['image']['full']}"
 
+        #WRONG ICONS AS OF 25/5/2020
+        # Vladimir(E & R), TwistedFate(PQWE)
+        # Shyvana(all), Zed(WER), Rammus(all), Orianna(R),
+        # Shaco(all), Qiyana(all), Vayne(R)
+
+        #FIXED AS OF 25/5/2020
+        # Ahri(P), AurelionSol(WE), Graves(PQR), Chogath(QE), Corki(P), Darius(All), Diana(All), Draven(E),
+        # Elise(all), Fiddlesticks(all), Gnar(R), Janna(R), Jax(all), Leblanc(P), LeeSin(all), Lulu(E),
+        # Lux(Q & E swapped), Malphite(W), Morgana(R), Naut(R & E swapped)
+
     # Special cases
-    KEY = key
-    if key == "gnar" and ability_key == "r":
+    if key.lower() == "gnar" and ability_key == "r":
         key = "gnarbig"
-    if key == "aurelionsol" and ability_key == "w":
-        ability_name = "startsin"
-    if key == "aurelionsol" and ability_key == "w":
-        ability_name = "startsin"
-    if key == "graves" and ability_key == "passive":
-        ability_name = "True Grit"
-    if key == "chogath" and ability_key == "passive":
-        ability_name = "Tailspike"
+    if key.lower() == "ahri" and ability_key == "passive":
+        ability_name = "souleater"
+    if key.lower() == "aurelionsol" and ability_key == "w":
+        ability_name = "starsin"
+    if key.lower() == "aurelionsol" and ability_key == "e":
+        ability_name = "fly"
+    if key.lower() == "graves" and ability_key == "passive":
+        ability_name = "truegrit"
+    if key.lower() == "graves" and ability_key == "q":
+        ability_name = "buckshot"
+    if key.lower() == "graves" and ability_key == "q":
+        ability_name = "highnoon"
+    if key.lower() == "chogath":
         key = "greenterror"
-    if key == "corki" and ability_key == "passive":
-        ability_name = "The Package"
-    if key == "corki" and ability_name == "Special Delivery":
-        ability_name = "Valkyrie Mega"
-    if key == "darius":
+        if ability_key == "passive":
+            ability_name = "tailspike"
+        if ability_key == "q":
+            ability_name = "spikeslam"
+        if ability_key == "e":
+            ability_name = "chitinousexoplates"
+    if key.lower() == "corki" and ability_key == "passive":
+        ability_name = "thepackage"
+    if key.lower() == "darius":
+        if ability_key == 'w':
+            ability_name = "hamstring"
+        if ability_key == 'e':
+            ability_name = "axe_grab"
+        if ability_key == 'r':
+            ability_name = "sudden_death"
         ability_name = "icon_" + ability_name
-    if key == "diana" and ability_key == "passive":
-        ability_name = "lunarblade"
-    if key == "elise" and ability_index == 2:
-        ability_name = "spider"  # leave off the real ability_name
-    if key == "jax":
+    if key.lower() == "diana":
+        if ability_key == "passive":
+            ability_name = "lunarblade"
+        if ability_key == "q":
+            ability_name = "moonsedge"
+        if ability_key == "w":
+            ability_name = "lunarshower"
+        if ability_key == "e":
+            ability_name = "fasterthanlight"
+        if ability_key == "r":
+            ability_name = "moonfall"
+    if key.lower() == "draven" and ability_key == "e":
+        ability_name = "twinaxe"
+    if key.lower() == "elise" and ability_key != "r":
+        key = "spiderhuman"  # leave off the real ability_name
+    if key.lower() == "fiddlesticks":
+        ability_name += ".fiddlesticksrework"
+    if key.lower() == "janna" and ability_key == "r":
+        ability_name = "reapthewhirlwind"
+    if key.lower() == "jax":
         key = "armsmaster"
-    if key == "jayce" and ability_name == "Shock Blast":
-        ability_name = "Shot Blast"
-    if key == "jayce" and ability_name == "Transform Mercury Hammer":
-        ability_name = "Transform Hammer"
-    if key == "leesin":
+        if ability_key == "passive":
+            ability_name = "masterofarms"
+        if ability_key == "q":
+            ability_name = "relentlessassault"
+        if ability_key == "w":
+            ability_name = "empower"
+        if ability_key == "e":
+            ability_name = "disarm"
+        if ability_key == "r":
+            ability_name = "coupdegrace"
+    if key.lower() == "leblanc" and ability_key == "passive":
+        ability_name = ""
+    if key.lower() == "leesin":
         key = "blindmonk"
-    if key == "blindmonk" and ability_index == 1:
-        ability_index = "one"
-    if key == "blindmonk" and ability_index == 2:
-        ability_index = "two"
-    if key == "morgana":
+        if ability_key != "passive" and ability_key != "r":
+            ability_key += "one"
+    if key.lower() == "lulu":
+        if ability_key == "q":
+            ability_name = "glitterbolt"
+        if ability_key == "e":
+            ability_name = "commandpix"
+        if ability_key == "r":
+            ability_name = "giantgrowth"
+    if key.lower() == "lux":
+        if ability_key == "q":
+            ability_name = "crashingblitz2"
+        if ability_key == "e":
+            ability_name = "lightstrikekugel"
+    if key.lower() == "malphite" and ability_key == "w":
+        ability_name = "brutalstrikes"
+    if key.lower() == "morgana":
         key = "fallenangel"
-    if key == "fallenangel" and ability_key == "passive":
-        ability_name = "Empathize"
-    if key == "rammus":
+        if ability_key == "passive":
+            ability_name = "empathize"
+        if ability_key == "r":
+            ability_name = "purgatory"
+    if key.lower() == "rammus":
         key = "armordillo"
-    if key == "armordillo" and ability_key == "passive":
+    if key.lower() == "armordillo" and ability_key == "passive":
         ability_name = "Scavenge Armor"
-    if key == "riven" and ability_key == "r":
+    if key.lower() == "riven" and ability_key == "r":
         ability_name = "Wind Scar"
-    if key == "shaco":
+    if key.lower() == "shaco":
         key = "jester"
-    if key == "jester" and ability_key == "passive":
+    if key.lower() == "jester" and ability_key == "passive":
         ability_name = "Careful Strikes"
-    if key == "shyvana" and ability_key == "passive":
+    if key.lower() == "shyvana" and ability_key == "passive":
         ability_name = "Reinforced Scales"
-    if key == "sion" and ability_name == "Death Surge":
+    if key.lower() == "sion" and ability_name == "Death Surge":
         ability_name = "passive2"
-    if key == "twistedfate":
+    if key.lower() == "twistedfate":
         key = "cardmaster"
-    if key == "cardmaster" and ability_key == "passive":
+    if key.lower() == "cardmaster" and ability_key == "passive":
         ability_name = "Seal Fate"
-    if key == "veigar" and ability_key == "passive":
+    if key.lower() == "veigar" and ability_key == "passive":
         ability_name = "Entropy"
-    if key == "ziggs" and ability_key == "passive":
+    if key.lower() == "ziggs" and ability_key == "passive":
         ability_name = "passiveready"
 
     best_fn, best_score, best_guess = perform_guess(
@@ -166,3 +229,4 @@ def get_ability_url(key, ability_key, ability_index, ability_name, latest_versio
         print(f"{key}-{ability_name}({ability_key}): {best_fn} ~ {best_guess}  {best_score}")
 
     return f"http://raw.communitydragon.org/latest/game/assets/characters/{KEY.lower()}/hud/icons2d/{best_fn}"
+    """
