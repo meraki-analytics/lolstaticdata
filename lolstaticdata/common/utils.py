fd2396bb0bb98e446cf296727abf7da9d0902ace
Jason Maldonis <jjmaldonis@gmail.com>
2020-12-12 17:58:48 +0000

    set user agent for requests

--- lolstaticdata/common/utils.py
+++ lolstaticdata/common/utils.py
@@ -128,7 +128,8 @@ def download_json(url: str, use_cache: bool = True) -> Json:
         with open(fn) as f:
             j = json.load(f)
     else:
-        page = requests.get(url)
+        headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+        page = requests.get(url, headers=headers)
         j = page.json()
         if use_cache:
             with open(fn, "w") as f:
