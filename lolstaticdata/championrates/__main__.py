9f8d327726dbb8870fa62ee6165daff0146c6016
dryancd <danryan426@gmail.com>
2021-04-19 10:46:22 -0400

    Blitz got rid of the graphql endpoint we were using. Swapped it to lolalytics Will add more functionality later but right now it gets the stats of the main role of every champ

--- lolstaticdata/championrates/__main__.py
+++ lolstaticdata/championrates/__main__.py
@@ -27,19 +27,26 @@ def main():
 }}
 &variables={{"language":"en","role":"ALL","region":"world","queue":420,"tier":"PLATINUM_PLUS","patch":"{patch}"}}
 """.format(patch=patch.name)
-    data = requests.get("https://flash.blitz.gg/graphql?query=" + urllib.parse.quote(query, safe="/()=&")).json()["data"]["lolChampionsListOverview"]
+    data2 = requests.get("https://apix1.op.lol/tierlist/7/?lane=default&patch=11.8&tier=platinum_plus&queue=420&region=all").json()
+    totalPicked = data2["pick"]
+   # data = requests.get("https://flash.blitz.gg/graphql?query=" + urllib.parse.quote(query, safe="/()=&")).json()["data"]["lolChampionsListOverview"]
 
     role_name_map = {"TOP": "TOP", "JUNGLE": "JUNGLE", "MID": "MIDDLE", "ADC": "BOTTOM", "SUPPORT": "UTILITY"}
+    roles = ["TOP", "JUNGLE", "MIDDLE", "BOTTOM", "UTILITY"]
 
     final = {}
-    for datum in data:
-        id = datum["champion_id"]
-        role = role_name_map[datum["role"]]
+    for datum in data2["cid"]:
+        id = datum
+        roleId = data2["cid"][datum][1]
+        role = roles[roleId-1]
+        wins = data2["cid"][datum][3]
+        totalChamp = data2["cid"][datum][4]
+
         final[id] = {
             role: {
-                "playRate": datum["stats"]["pickRate"],
-                "winRate": datum["stats"]["winRate"],
-                "banRate": datum["stats"]["banRate"],
+                "playRate": round(totalChamp/totalPicked,2),
+                "winRate": round(wins/totalChamp,2),
+                "banRate": data2["cid"][datum][6],
             }
         }
     for champion in cass.get_champions(region="NA"):
@@ -47,8 +54,6 @@ def main():
             final[champion.id] = {}
 
     final = {"data": final, "patch": patch.name}
-
-
     filename = "/home/meraki/code/meraki/Data/champion-rates/rates.json"
     with open(filename, "w") as f:
         json.dump(final, f)
