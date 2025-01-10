0ba92bb1233bd2fe9bf6b9572a1dacfe1be77b7a
Rico <rvzelst7@gmail.com>
2025-01-10 05:45:57 +0100

    fix: viktor missing spells (#102)
    Add Viktor missing spells to fix a crash when pulling champion data

--- lolstaticdata/champions/pull_champions_wiki.py
+++ lolstaticdata/champions/pull_champions_wiki.py
@@ -118,6 +118,7 @@ class LolWikiDataHandler:
         "Shaco": ["Command: Hallucinate"],
         "Syndra": ["Force of Will 2"],
         "Taliyah": ["Seismic Shove 2"],
+        "Viktor": ["Glorious Evolution 2", "Glorious Evolution 3", "Glorious Evolution 4", "Glorious Evolution 5", "Arcane Storm 2", "Arcane Storm 3"],
     }
 
     def __init__(self, use_cache: bool = True):
