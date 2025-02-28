5704ed70a762f7b1762768ee04f7c605790d8c4a
Ryan Davis <rdfiasco@gmail.com>
2025-02-28 00:52:01 -0700

    fix item property inheritance "=>"

--- lolstaticdata/champions/pull_champions_wiki.py
+++ lolstaticdata/champions/pull_champions_wiki.py
@@ -254,7 +254,7 @@ class LolWikiDataHandler:
                 or d["date"] == ""
                 or datetime.strptime(d["date"], "%Y-%m-%d") > datetime.today()
             ):  # Champion not released yet
-                print(f"Skipping unreleased champion {name}")
+                print(f"INFO: Skipping unreleased champion {name}")
                 continue
             champion = self._render_champion_data(name, d)
             yield champion
